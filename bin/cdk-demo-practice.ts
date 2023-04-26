93d4b3d5764ac0124c97640c1011b220b9baaee9
Praveen <praveenkilhore@gmail.com>
2023-04-26 16:41:15 +0530

    commit

--- bin/cdk-demo-practice.ts
+++ bin/cdk-demo-practice.ts
@@ -1,6 +1,11 @@
 #!/usr/bin/env node
+import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { CdkDemoPracticeStack } from '../lib/cdk-demo-practice-stack';
 
+// const envUSA  = { account: 'accountno', region: 'us-central-1' };
+
 const app = new cdk.App();
-new CdkDemoPracticeStack(app, 'CdkDemoPracticeStack');
+new CdkDemoPracticeStack(app, 'CdkDemoPracticeStack', {
+  // env: envUSA
+});
--- lib/cdk-demo-practice-stack.ts
+++ lib/cdk-demo-practice-stack.ts
@@ -1,19 +1,49 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
-import { Construct } from 'constructs';
+import * as cdk from 'aws-cdk-lib';
+import { aws_s3 as s3 } from 'aws-cdk-lib';
+import { aws_ec2 as ec2 } from 'aws-cdk-lib';
+import { PrivateSubnet } from 'aws-cdk-lib/aws-ec2';
 
-export class CdkDemoPracticeStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+export class CdkDemoPracticeStack extends cdk.Stack {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'CdkDemoPracticeQueue', {
-      visibilityTimeout: Duration.seconds(300)
+    // const s3bucket = new s3.Bucket(this, 'DemoBucket', {
+    //   versioned: true
+    // });
+
+    const vpc = new ec2.Vpc(this, 'DemoVPC', {
+      cidr: "10.0.0.0/16",
+      natGateways: 0,
+      subnetConfiguration: [{
+        cidrMask: 24,
+        name: 'private_Subnet',
+        subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+      }, {
+        cidrMask: 24,
+        name: 'public_Subnet',
+        subnetType: ec2.SubnetType.PUBLIC,
+      }]
     });
 
-    const topic = new sns.Topic(this, 'CdkDemoPracticeTopic');
+    var publicSubnets = vpc.publicSubnets;
+    var privateSubnets = vpc.isolatedSubnets;
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    const subnet_for_ec2Instance: ec2.SubnetSelection = { subnets: publicSubnets };
+    const subnet_for_endpoint: ec2.SubnetSelection = { subnets: privateSubnets };
+
+    new ec2.Instance(this, 'Instance', {
+      vpc: vpc,
+      vpcSubnets: subnet_for_ec2Instance,
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE2, ec2.InstanceSize.MICRO),
+      machineImage: ec2.MachineImage.latestAmazonLinux2022()
+    });
+
+    // new ec2.InterfaceVpcEndpoint(this, 'VPCEndpoint', {
+    //   vpc: vpc,
+    //   //(name of the service, port)
+    //   service: new ec2.InterfaceVpcEndpointService('com.amazonaws.vpce.us-east-1.vpce-svc-uuddlrlrbastrtsvc', 443),
+    //   subnets: subnet_for_endpoint
+    // });
+
   }
 }
